1d8b5ef3bf49a830b0bc8be2f381f9254b676570
Can <ozbekahmetcan@gmail.com>
2019-01-31 02:27:12 -0800

    add util folder

---
+++ util/settings.py
@@ -0,0 +1,7 @@
+import os
+
+PRH_HOST = os.environ.get('PRH_HOST')
+PRH_USER = os.environ.get('PRH_USER')
+PRH_PASSWORD = os.environ.get('PRH_PASSWORD')
+PRH_PORT = os.environ.get('PRH_PORT')
+PRH_DBNAME = os.environ.get('PRH_DBNAME')

3ffab6af2c6e23070533c0b367e7b7dc435d1d3e
Can <ozbekahmetcan@gmail.com>
2019-02-02 01:53:29 -0800

    add generate-cf

--- ph_likes/util/settings.py
+++ ph_likes/util/settings.py
@@ -1,9 +1,14 @@
 import os
 
+# db
 PRH_HOST = os.environ.get('PRH_HOST')
 PRH_USER = os.environ.get('PRH_USER')
 PRH_PASSWORD = os.environ.get('PRH_PASSWORD')
 PRH_PORT = os.environ.get('PRH_PORT')
 PRH_DBNAME = os.environ.get('PRH_DBNAME')
 
-EXAMPLE_USER_ID = 256379
+EXAMPLE_USER_ID = 891
+
+# 
+LOCAL_DATA_PATH = os.environ.get('PRH_LOCAL_DATA_PATH')
+LOCAL_MODELS_PATH = os.environ.get('PRH_LOCAL_MODELS_PATH')
